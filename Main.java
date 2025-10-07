cc704d20ac10e4304f14e98aaca16aff6a9b6b11
kimi1273698276342691734617364e <kimthai002@gmail.com>
2025-10-07 21:46:14 +0000

    problem 2

--- Main.java
+++ Main.java
@@ -6,10 +6,24 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
+    // Problem 1
+    System.out.println("Enter in a number");
+    double target = 12.345;
+    double user_input = scan.nextDouble();
 
+    if (user_input == target)
+    {
+      System.out.println("YES!");
+    }
 
-
-
+  // Problem 2
+  System.out.println("Please enter an integer");
+  int targInt = 48;
+  int Num = scan.nextInt();
+  if(Num == targInt)
+  {
+    System.out.println("YES!");
+  }
     /* ============= END SOLUTION ================================== */
     scan.close();
   }
